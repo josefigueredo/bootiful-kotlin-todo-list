f7cf86ac988f62726c37eeaef20047b5cf66b5cd
mrFlick72 <Back72Star>
2018-07-28 10:21:53 +0200

    graphics updates

--- src/main/frontend/app/component/NewTodoItem.js
+++ src/main/frontend/app/component/NewTodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({newTodoInputRef, newTodoInputOnClickHandler, buttonText}) => {
     return (<div className="input-group mb-3">
-        <textarea className="text-area" ref={newTodoInputRef}/>
+        <textarea className="form-control" ref={newTodoInputRef}/>
         <div className="input-group-append">
             <span className="input-group-text" onClick={newTodoInputOnClickHandler}> {buttonText}</span>
         </div>
--- src/main/frontend/app/component/UpdateTodoItemPopUp.js
+++ src/main/frontend/app/component/UpdateTodoItemPopUp.js
@@ -2,22 +2,25 @@ import React from 'react';
 
 export default ({modalId, updateTodoItem, updateTodoInputRef}) => {
     return (<div id={modalId} className="modal" tabIndex="-1" role="dialog">
-        <div className="modal-dialog" role="document">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h5 className="modal-title">Modal title</h5>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div className="modal-body">
-                    <textarea className="text-area" ref={updateTodoInputRef}/>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" className="btn btn-primary" onClick={updateTodoItem}>Save changes</button>
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Update Todo Item</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="form-group">
+                            <textarea className="form-control" ref={updateTodoInputRef}/>
+                        </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary" onClick={updateTodoItem}>Save changes</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>)
+    )
 }
